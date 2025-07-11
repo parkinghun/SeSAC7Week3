cc017e6a40033e13cafe678895441c105ea3df09
Sunghun Park <dfstem9@gmail.com>
2025-07-11 11:32:04 +0900

    MovieTableViewCell 파일 추가

---
+++ SeSAC7Week3/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  SeSAC7Week3
+//
+//  Created by 박성훈 on 7/11/25.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
